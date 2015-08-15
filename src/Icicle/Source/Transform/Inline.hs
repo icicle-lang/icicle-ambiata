81a85b581446868095ae8f77a99639f718b18aa3
Amos Robinson <amos.robinson@ambiata.com>
2015-08-15 11:21:59 +1000

    Source: inline transform, substitution, etc

---
+++ src/Icicle/Source/Transform/Inline.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE PatternGuards #-}
+module Icicle.Source.Transform.Inline (
+    inlineTransform
+  , inlineQ
+  ) where
+
+import Icicle.Source.Query
+import Icicle.Source.Transform.Base
+import Icicle.Source.Transform.SubstX
+
+import Icicle.Common.Base
+import Icicle.Common.Fresh
+
+import P
+
+import              Data.Functor.Identity
+import              Data.List (zip)
+import qualified    Data.Map as Map
+
+
+
+inlineTransform
+        :: Ord n
+        => Map.Map (Name n) (Function a n)
+        -> Transform (Fresh n) () a n
+inlineTransform funs
+ = Transform
+ { transformExp     = tranx
+ , transformContext = tranc
+ , transformState   = ()
+ }
+ where
+  tranx _ x
+   | (Var a n, args) <- takeApps x
+   , Just fun        <- Map.lookup n funs
+   , length args == length (arguments fun)
+   = do let argNames
+                    = fmap snd $ arguments fun
+        ns         <- mapM freshPrefix' argNames
+
+        let sub     = Map.fromList
+                    $ argNames `zip` fmap (Var a) ns
+        let sub'    = unsafeSubstTransform
+                    { transformState = sub }
+
+        let body'   = runIdentity
+                    $ transformQ sub'
+                    $ body fun
+
+        let lets = fmap (mkLet a) (ns `zip` args)
+        return ((), Nested a (prefixContexts lets body'))
+
+   | otherwise
+   = return ((), x)
+
+  tranc _ c
+   = return ((), c)
+
+  mkLet a (n,d) = Let a n d
+
+  prefixContexts cs (Query cs' xx)
+   = Query (cs <> cs') xx
+
+
+inlineQ :: Ord n
+        => Map.Map (Name n) (Function a n)
+        -> Query a n
+        -> Fresh n (Query a n)
+inlineQ funs
+ = transformQ (inlineTransform funs)
---
+++ src/Icicle/Source/Transform/SubstX.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE PatternGuards #-}
+module Icicle.Source.Transform.SubstX (
+    unsafeSubstTransform
+  ) where
+
+import Icicle.Source.Query
+import Icicle.Source.Transform.Base
+
+import Icicle.Common.Base
+
+import P
+
+import Data.Functor.Identity
+
+import qualified Data.Map as Map
+
+
+-- | This is unsafe in the sense that it does not avoid capture of the payload.
+-- It is fine if the only variables in the payload are fresh:
+-- eg 'subst x := (5 + fresh) in x + x'
+-- because we know there can be no mention of "fresh" in the target.
+
+unsafeSubstTransform
+    :: Ord n
+    => Transform Identity (Map.Map (Name n) (Exp a n)) a n
+
+unsafeSubstTransform
+ = Transform
+ { transformExp     = tranx
+ , transformContext = tranc
+ , transformState   = Map.empty
+ }
+ where
+  tranx s x
+   = case x of
+      Var _ n
+       | Just x' <- Map.lookup n s
+       -> return (s, x')
+      _
+       -> return (s, x)
+
+  tranc s c
+   = case c of
+      -- To make this safe, we would need to
+      -- look through the free vars of the map's values (the payloads)
+      -- and rename this binding if any payloads
+      -- refer to a variable with the same name
+      Let _ n _
+       -> return (Map.delete n s, c)
+      LetFold _ f
+       -> return (Map.delete (foldBind f) s, c)
+      _
+       -> return (s, c)
+
