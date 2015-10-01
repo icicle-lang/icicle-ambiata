24e87bbfb7ee12c182f1df23cbff24caf8067ad9
Amos Robinson <amos.robinson@ambiata.com>
2015-10-01 19:22:29 +1000

    Wip: ReifyPossibility

--- src/Icicle/Source/Transform/ReifyPossibility.hs
+++ src/Icicle/Source/Transform/ReifyPossibility.hs
@@ -30,5 +30,46 @@ reifyPossibilityTransform
   tranx _ x
    = return ((), x)
   tranc _ c
-   = return ((), c)
+   = case c of
+      LetFold a f
+       | FoldTypeFoldl1 <- foldType f
+       -> do  nError <- fresh
+              nValue <- fresh
+              let b  = foldBind f
+                  a' = a { annResult = canonT $ SumT ErrorT $ annResult a }
+
+                  z' = con1 a' ConLeft $ ConError ErrorEmptyFold1
+
+                  -- Will need to desugar after this
+                  k' = Case a' b
+                     [ ( PatCon ConLeft  [ PatCon $ ConError ErrorEmptyFold1 ]
+                       , wrapRight $ foldZero f )
+                     , ( PatCon ConLeft  [ PatVariable nError ]
+                       , con1 a' ConLeft $ Var a' nError )
+                     , ( PatCon ConRight [ PatVariable nValue ]
+                       , wrapRight
+                       $ wrapBareInput nValue
+                       $ foldKons f ) ]
+
+                  f' = f { foldType = FoldTypeFoldl
+                         , foldZero = z'
+                         , foldKons = k' }
+
+              return ((), LetFold a' f')
+
+      _
+       -> return ((), c)
+
+  con1 a c x = App a (Prim a (PrimCon c)) x
+
+  wrapRight x
+   | ann        <- annotOfExp x
+   , t          <- annResult  ann
+   , Possibly   <- getPossibilityOrDefinitely t
+   = con1 (ann { annResult = canonT $ SumT ErrorT t) ConRight x
+   | otherwise
+   = x
+
+  wrapBareInput n x
+   = ...
 
