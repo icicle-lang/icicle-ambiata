dd876ee85eed42cb932ec20293c9681b5d72b53a
Tran Ma <tran@defma.in>
2015-10-06 10:57:21 +1100

    source: damn missing apostrophe

--- test/Icicle/Test/Source/Convert.hs
+++ test/Icicle/Test/Source/Convert.hs
@@ -120,4 +120,4 @@ xfst tt
 return []
 tests :: IO Bool
 -- tests = $quickCheckAll
-tests = $forAllProperties $ quickCheckWithResult (stdArgs { maxSuccess = 5000, maxSize = 10, maxDiscardRatio = 10000})
+tests = $forAllProperties $ quickCheckWithResult (stdArgs { {- maxSuccess = 5000, -} maxSize = 10, maxDiscardRatio = 10000})
