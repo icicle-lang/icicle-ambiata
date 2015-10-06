1ef133b0622316116a3f0b89706b3e60eaa6bf2d
Tran Ma <tran@defma.in>
2015-10-06 13:48:45 +1100

    source: fix checker

--- src/Icicle/Source/Checker/Function.hs
+++ src/Icicle/Source/Checker/Function.hs
@@ -22,6 +22,7 @@ import                  Control.Monad.Trans.Either
 
 import qualified        Data.Map                as Map
 import qualified        Data.Set                as Set
+import qualified        Data.List               as List
 
 checkFs :: Ord n
         => Map.Map (Name n) (FunctionType n, Function (Annot a n) n)
@@ -59,10 +60,15 @@ checkF' fun env cons
  = do -- Give each argument a fresh type variable
       env' <- foldM bindArg env $ arguments fun
       -- Get the annotated body
-      (q',_,cons')  <- generateQ (body fun) env' cons
+      (q', subs, cons')  <- generateQ (body fun) env' cons
+
+      -- Look up the argument types after solving all constraints.
+      -- Because they started as fresh unification variables,
+      -- they will end up being unified to the actual types.
+      (args, cs) <- List.unzip <$> mapM (lookupArg subs env' cons') (arguments fun)
 
       -- Find all leftover constraints and nub them
-      let constrs = ordNub $ fmap snd cons'
+      let constrs = ordNub $ fmap snd (concat cs)
 
       -- We want to remove any modes (temporalities or possibilities)
       -- that are bound by foralls with no constraints on them.
@@ -117,11 +123,6 @@ checkF' fun env cons
            = let (tmp,pos,dat) = decomposeT t
              in  recomposeT (remode tmp, remode pos, dat)
 
-      -- Look up the argument types after solving all constraints.
-      -- Because they started as fresh unification variables,
-      -- they will end up being unified to the actual types.
-      args <- mapM (lookupArg env' cons') $ arguments fun
-
       -- Fix the modes of all the argument and result types
       let argTs = fmap (fixmodes . annResult . fst) args
       let resT  = fixmodes $ annResult $ annotOfQuery q'
@@ -145,7 +146,7 @@ checkF' fun env cons
    <*> (Possibility <$> (TypeVar <$> fresh)
    <*>                  (TypeVar <$> fresh))
 
-  lookupArg e c (a,n)
-   = do (_,_,t,_) <- lookup a n e c
-        return (Annot a t [], n)
+  lookupArg subs e c (a,n)
+   = do (_,_,t,c') <- lookup a n e c
+        return ((Annot a (substT subs t) [], n), c')
 
