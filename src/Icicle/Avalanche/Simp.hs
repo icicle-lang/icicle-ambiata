4cf47a6730eb45c650888fc75a3592db9a0075d1
Amos Robinson <amos.robinson@ambiata.com>
2015-11-17 11:32:07 +1100

    Avalanche: constant evaluation on primitives for both Avalanche.Core and Avalanche.Flat

--- src/Icicle/Avalanche/Simp.hs
+++ src/Icicle/Avalanche/Simp.hs
@@ -10,8 +10,13 @@ module Icicle.Avalanche.Simp (
 import              Icicle.Common.Exp
 import              Icicle.Common.Fresh
 
-import              Icicle.Avalanche.Prim.Flat
+import qualified    Icicle.Core.Exp.Prim as CorePrim
+import qualified    Icicle.Core.Eval.Exp as CorePrim
+
+import qualified    Icicle.Avalanche.Prim.Eval as Flat
+import qualified    Icicle.Avalanche.Prim.Flat as Flat
 import              Icicle.Avalanche.Statement.Simp
+import              Icicle.Avalanche.Statement.Simp.Eval
 import              Icicle.Avalanche.Statement.Simp.Constructor
 import              Icicle.Avalanche.Statement.Simp.Melt
 import              Icicle.Avalanche.Program
@@ -20,10 +25,10 @@ import              P
 
 
 simpAvalanche
-  :: (Show n, Show p, Ord n, Eq p)
+  :: (Show n, Ord n)
   => a
-  -> Program a n p
-  -> Fresh n (Program a n p)
+  -> Program a n CorePrim.Prim
+  -> Fresh n (Program a n CorePrim.Prim)
 simpAvalanche a_fresh p
  = do p' <- transformX return (simp a_fresh) p
       s' <- (forwardStmts a_fresh $ pullLets $ statements p')
@@ -31,14 +36,15 @@ simpAvalanche a_fresh p
          >>= forwardStmts a_fresh
          >>= nestBlocks   a_fresh
          >>= thresher     a_fresh
+         >>= transformX return (return . simpEvalX CorePrim.evalPrim CorePrim.typeOfPrim)
 
       return $ p { statements = s' }
 
 simpFlattened
   :: (Show n, Ord n, Eq a)
   => a
-  -> Program a n Prim
-  -> Fresh n (Program a n Prim)
+  -> Program a n Flat.Prim
+  -> Fresh n (Program a n Flat.Prim)
 simpFlattened a_fresh p
  = do s' <- transformX return (simp a_fresh) (statements p)
          >>= melt a_fresh
@@ -59,6 +65,8 @@ simpFlattened a_fresh p
    >>= thresher     a_fresh
    -- Perform let-forwarding on statements, so that constant lets become free
    >>= forwardStmts a_fresh
+   -- Try to evaluate any exposed primitives
+   >>= transformX return (return . simpEvalX Flat.evalPrim Flat.typeOfPrim)
    -- Pull Let statements out of blocks. This just allows thresher to remove more duplicates
    >>= nestBlocks   a_fresh
    -- Thresh again. Surprisingly, having both threshers makes simpFlattened twice as fast!
---
+++ src/Icicle/Avalanche/Statement/Simp/Eval.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE PatternGuards     #-}
+module Icicle.Avalanche.Statement.Simp.Eval
+     ( simpEvalX
+     , simpEvalP
+     ) where
+
+import           Icicle.Common.Value
+import           Icicle.Common.Exp
+import           Icicle.Common.Type
+
+import           P
+
+
+simpEvalX   :: Ord n
+            => EvalPrim a n p
+            -> (p -> Type)
+            -> Exp a n p
+            -> Exp a n p
+simpEvalX ev ty = fixp (simpEvalX' ev ty)
+ where
+  fixp f x
+   | Just x' <- f x = fixp f x'
+   | otherwise      = x
+
+
+simpEvalX'  :: Ord n
+            => EvalPrim a n p
+            -> (p -> Type)
+            -> Exp a n p
+            -> Maybe (Exp a n p)
+simpEvalX' ev ty = go
+  where
+    go xx = case xx of
+      XApp a p q
+        | mp <- go p
+        , mq <- go q
+        , p' <- fromMaybe p mp
+        , q' <- fromMaybe q mq
+        , x' <- XApp a p' q'
+        , Just (prim, as) <- takePrimApps x'
+        , Just args       <- mapM (takeValue . just go) as
+        -> case simpEvalP ev ty a prim args of
+            Just x''
+             -> return x''
+            Nothing
+             | isJust mp || isJust mq
+             -> return x'
+             | otherwise
+             -> Nothing
+
+      XApp a p q
+        | Just p' <- go p, Just q' <- go q -> Just $ XApp a p' q'
+        | Just p' <- go p, Nothing <- go q -> Just $ XApp a p' q
+        | Nothing <- go p, Just q' <- go q -> Just $ XApp a p  q'
+        | otherwise                        -> Nothing
+
+      XLam a n t x1
+        -> XLam a n t <$> go x1
+
+      XLet a n p q
+        | Just p' <- go p, Just q' <- go q -> Just $ XLet a n p' q'
+        | Just p' <- go p, Nothing <- go q -> Just $ XLet a n p' q
+        | Nothing <- go p, Just q' <- go q -> Just $ XLet a n p  q'
+        | otherwise                        -> Nothing
+
+      XVar{}   -> Nothing
+      XPrim{}  -> Nothing
+      XValue{} -> Nothing
+
+    just f x = fromMaybe x (f x)
+
+
+-- | Primitive Simplifier
+--
+simpEvalP   :: Ord n
+            => EvalPrim a n p
+            -> (p -> Type)
+            -> a
+            -> p
+            -> [Value a n p]
+            -> Maybe (Exp a n p)
+simpEvalP ev ty a_fresh p vs
+ = case ev p vs of
+    Right (VBase b)
+     -> Just
+      $ XValue a_fresh (functionReturns $ ty p) b
+    -- TODO: we could actually pull the
+    -- heap out as let bindings, and so on..
+    Right VFun{}
+     -> Nothing
+    Left _
+     -> Nothing
+
