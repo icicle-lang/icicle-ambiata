79cdb356fbd2486e30ae127a7691281511231149
Jacob Stanley <jacob@stanley.io>
2015-12-10 11:32:39 +1100

    Add a bunch more benchmarks

--- bench/bench.hs
+++ bench/bench.hs
@@ -6,24 +6,26 @@ import           Control.Monad.IO.Class (liftIO)
 import           Criterion.Main
 import           Criterion.Types (Config(..))
 
+import           Data.Char (toLower)
 import qualified Data.Map as Map
+import           Data.Maybe (fromJust)
+import           Data.String (String)
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import           Data.Maybe (fromJust)
-import           Data.String (String)
 
 import qualified Icicle.Benchmark as I
-import           Icicle.Benchmark.Generator (FileSpec(..), AttributeType(..), generateSparse)
+import           Icicle.Benchmark.Generator (AttributeType(..), Frequency(..))
+import           Icicle.Benchmark.Generator (Generator(..), generateSparse)
 import           Icicle.Data.Time (timeOfText)
 import qualified Icicle.Sea.Eval as I
 
 import           P
 
-import           System.Directory (copyFile)
+import           System.Directory (createDirectoryIfMissing, copyFile)
 import           System.Exit (ExitCode(..))
 import           System.FilePath (FilePath, (</>))
-import           System.IO (IO, putStrLn)
+import           System.IO (IO, hPutStrLn, stderr)
 import           System.IO.Temp (withSystemTempDirectory)
 import           System.Process (system)
 
@@ -35,17 +37,21 @@ import           X.Control.Monad.Catch
 main :: IO ()
 main =
   withSystemTempDirectory "icicle-bench-" $ \base -> do
-    putStrLn "Generating repos"
-    generateRepo repoSpec base
+    hPutStrLn stderr "Generating repos"
+    repos <- generateRepos base
+      -- frequency / duration (yrs) / entities
+      [ (PerMinute,  2,      10)
+      , (PerHour,    5,     200)
+      , (PerDay,    10,    1000)
+      , (PerDay,    10,    2000)
+      , (PerDay,    10,    3000)
+      , (PerMonth,   1, 1000000)
+      ]
 
-    putStrLn "Compiling"
-    _ <- runEitherT . bracketEitherT' (createBenchmark base) I.releaseBenchmark $ \b -> do
-      liftIO $ putStrLn "Running benchmarks"
-      liftIO $ defaultMainWith benchConfig
-        [ bgroup "entities=10"
-          [ group "years=2" b
-          ]
-        ]
+    hPutStrLn stderr "Compiling"
+    _ <- runEitherT . bracketEitherT' (createBenchmarks repos) releaseBenchmarks $ \bs -> do
+      liftIO $ hPutStrLn stderr "Running benchmarks"
+      liftIO $ defaultMainWith benchConfig (fmap group bs)
 
     return ()
 
@@ -58,8 +64,8 @@ benchConfig =
 
 ------------------------------------------------------------------------
 
-group :: String -> I.Benchmark -> Benchmark
-group name b = do
+group :: (String, I.Benchmark) -> Benchmark
+group (name, b) = do
   bgroup name
     [ bench "icicle" $ nfIO (icicle b)
     , bench "cat"    $ nfIO (cat (I.benchInputPath b))
@@ -70,7 +76,7 @@ group name b = do
 icicle :: I.Benchmark -> IO ()
 icicle b = do
   Right stats <- runEitherT (I.runBenchmark b)
-  putStrLn ("  " <> show stats)
+  hPutStrLn stderr ("  " <> show stats)
 
 cat :: FilePath -> IO ()
 cat path = do
@@ -79,41 +85,64 @@ cat path = do
 
 wcl :: FilePath -> IO ()
 wcl path = do
-  ExitSuccess <- system ("LANG=C LC_ALL=C wc -l " <> path)
+  ExitSuccess <- system ("LANG=C LC_ALL=C wc -l " <> path <> " >/dev/null")
   return ()
 
 wc :: FilePath -> IO ()
 wc path = do
-  ExitSuccess <- system ("LANG=C LC_ALL=C wc " <> path)
+  ExitSuccess <- system ("LANG=C LC_ALL=C wc " <> path <> " >/dev/null")
   return ()
 
 ------------------------------------------------------------------------
 
-generateRepo :: FileSpec -> FilePath -> IO ()
-generateRepo spec path = do
-  putStrLn ("  " <> show spec)
-  generateSparse spec (path </> "data.psv")
+type Years    = Int
+type Entities = Int32
+
+generateRepos :: FilePath -> [(Frequency, Years, Entities)] -> IO [(String, FilePath)]
+generateRepos root = traverse (generateRepo root)
+
+generateRepo :: FilePath -> (Frequency, Years, Entities) -> IO (String, FilePath)
+generateRepo root (freq, ys, es) = do
+  let generator = Generator
+        { genSeed        = 12345678
+        , genEntityCount = es
+        , genAttributes  = Map.fromList [("wibble", Int)]
+        , genStart       = 2015 - (ys - 1)
+        , genEnd         = 2015
+        , genFrequency   = freq }
+
+      name = "frequency=" <> fmap toLower (drop 3 (show freq))
+         </> "duration="  <> show ys <> "yrs"
+         </> "entities="  <> show es
+
+      path = root </> name
+
+  hPutStrLn stderr ("  " <> show generator)
+
+  createDirectoryIfMissing True path
+  generateSparse generator (path </> "data.psv")
+
   T.writeFile (path </> "dictionary.toml") dictionary
   copyFile "data/libs/prelude.icicle" (path </> "prelude.icicle")
 
-createBenchmark :: FilePath -> EitherT I.BenchError IO I.Benchmark
-createBenchmark path =
-  I.createBenchmark mode (path </> "dictionary.toml")
-                         (path </> "data.psv")
-                         (path </> "out.psv")
-                         Nothing
+  return (name, path)
+
+createBenchmarks :: [(String, FilePath)] -> EitherT I.BenchError IO [(String, I.Benchmark)]
+createBenchmarks = traverse createBenchmark
+
+createBenchmark :: (String, FilePath) -> EitherT I.BenchError IO (String, I.Benchmark)
+createBenchmark (name, path) = do
+  let dict   = (path </> "dictionary.toml")
+      input  = (path </> "data.psv")
+      output = (path </> "out.psv")
+  b <- I.createBenchmark mode dict input output Nothing
+  return (name, b)
+
+releaseBenchmarks :: [(String, I.Benchmark)] -> EitherT I.BenchError IO ()
+releaseBenchmarks = traverse_ I.releaseBenchmark . fmap snd
 
 mode :: I.PsvMode
 mode = I.PsvSnapshot (fromJust (timeOfText "2015-10-01"))
-
-repoSpec :: FileSpec
-repoSpec = FileSpec {
-    specSeed         = 12345678
-  , specEntityCount  = 10
-  , specAttributes   = Map.fromList [("wibble", Int)]
-  , specStart        = 2014
-  , specEnd          = 2015
-  }
 
 dictionary :: Text
 dictionary = T.unlines
