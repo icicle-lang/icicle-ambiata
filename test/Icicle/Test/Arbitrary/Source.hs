480eb764a3a08e44eddb882a36b9f3f058f82c5b
Amos Robinson <amos.robinson@gmail.com>
2016-04-04 14:15:36 +1000

    Smarter Source generator: still generates bad things, but somewhat less often

--- test/Icicle/Test/Arbitrary/Source.hs
+++ test/Icicle/Test/Arbitrary/Source.hs
@@ -44,7 +44,6 @@ import qualified Data.Map as Map
 import           Data.Functor.Identity
 
 import           Data.String   (String)
-import           Data.Hashable (Hashable)
 
 import           X.Control.Monad.Trans.Either
 
@@ -53,63 +52,10 @@ instance Arbitrary T.Variable where
  arbitrary
   = T.Variable <$> elements muppets
 
-instance (Arbitrary n, Hashable n) => Arbitrary (Exp () n) where
+instance Arbitrary (Exp () T.Variable) where
  arbitrary
-  = oneof_sized
-        [ Var    () <$> arbitrary
-        , Prim   () <$> arbitrary ]
-        [ (simplifyNestedX . Nested ()) <$> arbitrary
-        , App    () <$> arbitrary <*> arbitrary
-        , Case   () <$> arbitrary
-                    <*> arbitrary `suchThat` (not . null)
-        , preop
-        , inop
-        , builtins
-        ]
-
-  where
-   preop
-    = App ()
-    <$> (Prim () <$> operator_pre)
-    <*> arbitrary
-
-   inop
-    = App ()
-    <$> (App ()
-            <$> (Prim () <$> operator_bin)
-            <*> arbitrary)
-    <*> arbitrary
-
-   operator_bin
-    = oneof_vals
-        [ Op    (ArithDouble Div)
-        , Op    (ArithBinary Mul)
-        , Op    (ArithBinary Add)
-        , Op    (ArithBinary Sub)
-        , Op    (ArithBinary Pow)
-        , Op    (Relation Gt)
-        , Op    (Relation Eq)
-        , Op    (LogicalBinary And)
-        , Op    (LogicalBinary Or)
-        , Op    (TimeBinary DaysBefore)
-        , Op    (TimeBinary DaysAfter)
-        , Op    (TimeBinary MonthsBefore)
-        , Op    (TimeBinary MonthsAfter)
-        , Fun   (BuiltinTime DaysBetween)
-        , Fun   (BuiltinTime DaysEpoch)
-        , Fun   (BuiltinData Seq) ]
-
-   operator_pre
-    = oneof_vals
-        [ Op (ArithUnary Negate)
-        , Op (LogicalUnary Not)]
-
-   builtins
-    =   App ()
-    <$> (Prim () <$> oneof_vals
-          [ Fun (BuiltinMap MapKeys)
-          , Fun (BuiltinMap MapValues) ])
-    <*> arbitrary
+  = do tt <- arbitrary
+       genExp $ TypedGenInfo Map.empty tt
 
 instance Arbitrary Prim where
  arbitrary
@@ -128,43 +74,6 @@ instance Arbitrary Constructor where
  arbitrary
   = oneof_vals [ ConSome, ConNone, ConTuple, ConTrue, ConFalse, ConLeft, ConRight ]
 
-instance (Arbitrary n, Hashable n) => Arbitrary (Pattern n) where
- arbitrary
-  = oneof_sized [ return PatDefault, PatVariable <$> arbitrary ]
-                [ patcon ]
-  where
-   patcon
-    = do con  <- arbitrary
-         args <- List.take (arityOfConstructor con) <$> (infiniteList :: Gen [Pattern n])
-         return (PatCon con args)
-
-
-instance (Arbitrary n, Hashable n) => Arbitrary (Context () n) where
- arbitrary
-  = oneof_sized
-        [ Windowed () <$> arbitrary <*> arbitrary
-        , Latest   () . abs <$> arbitrary ]
-        [ GroupBy  () <$> arbitrary
-        , Distinct () <$> arbitrary
-        , Filter   () <$> arbitrary
-        , LetFold  () <$> arbitrary
-        , Let      () <$> arbitrary <*> arbitrary
-        -- no reason to generate group-folds that are not on groups
-        , GroupFold ()
-            <$> arbitrary
-            <*> arbitrary
-            <*> (Nested () <$> arbitrary `suchThat` hasGroup)
-        ]
-  where
-   hasGroup q
-    | (GroupBy _ _ : _) <- contexts q = True
-    | otherwise = False
-
-
-instance (Arbitrary n, Hashable n) => Arbitrary (Fold (Query () n) () n) where
- arbitrary
-  = Fold <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
-
 instance Arbitrary FoldType where
  arbitrary
   = oneof
@@ -172,14 +81,14 @@ instance Arbitrary FoldType where
         , return FoldTypeFoldl ]
 
 
-instance (Arbitrary n, Hashable n) => Arbitrary (Query () n) where
+instance Arbitrary (Query () T.Variable) where
  arbitrary
-  = Query <$> arbitrary <*> arbitrary
+  = do tt <- arbitrary
+       genQuery $ TypedGenInfo Map.empty tt
 
-instance (Arbitrary n, Hashable n) => Arbitrary (QueryTop () n) where
+instance Arbitrary (QueryTop () T.Variable) where
  arbitrary
   = QueryTop <$> arbitrary <*> arbitrary <*> arbitrary
-
 
 freshnamer t = counterPrefixNameState (T.Variable . Text.pack . show) (T.Variable t)
 
@@ -234,21 +143,46 @@ qwfQueryTop qwf
  = QueryTop (qwfFeatureN qwf) (qwfOutput qwf) (qwfQuery qwf)
 
 instance Arbitrary QueryWithFeature where
- arbitrary
-  = do q   <- arbitrary
-       now <- arbitrary
-       o   <- arbitrary
-       -- Convert to Source type and back, because this filters out Bufs.
-       -- (It actually converts them to Arrays)
-       -- This is fine, because Buf can't be in feature types.
-       --
-       -- This was causing issues with conversion from Source, because
-       -- part of the resulting Core was using the ValType with Bufs, and
-       -- part was using the Source-converted type with Arrays.
-       Just t <- (valTypeOfType . typeOfValType) <$> arbitrary
-       nm  <- arbitrary `suchThat` (\n -> Just n /= now)
-       tm  <- arbitrary `suchThat` (\n -> Just n /= now)
-       return $ QueryWithFeature q now o t nm tm
+ arbitrary = genQueryWithFeatureTypedGen
+
+genQueryWithFeatureTypedGen :: Gen QueryWithFeature
+genQueryWithFeatureTypedGen
+ = do namebase <- elements muppets
+      let make f = CB.nameOf $ CB.NameBase $ T.Variable (namebase <> f)
+      let now = make "now"
+      let nm  = make "featurename"
+      let tm  = make "timename"
+      let tgi = TypedGenInfo
+              { tgiVars = Map.fromList [(now, TTAgg), (nm, TTElt), (tm, TTElt)]
+              , tgiTemp = TTAgg }
+      q   <- genQuery tgi
+
+      o   <- arbitrary
+      -- Note: Convert to Source type and back in generator
+      -- Convert to Source type and back, because this filters out Bufs.
+      -- (It actually converts them to Arrays)
+      -- This is fine, because Buf can't be in feature types.
+      --
+      -- This was causing issues with conversion from Source, because
+      -- part of the resulting Core was using the ValType with Bufs, and
+      -- part was using the Source-converted type with Arrays.
+      Just t <- (valTypeOfType . typeOfValType) <$> arbitrary
+      return $ QueryWithFeature q (Just now) o t nm tm
+
+genQueryWithFeatureArbitrary :: Gen QueryWithFeature
+genQueryWithFeatureArbitrary
+ = do namebase <- elements muppets
+      let make f = CB.nameOf $ CB.NameBase $ T.Variable (namebase <> f)
+      let now = make "now"
+      let nm  = make "featurename"
+      let tm  = make "timename"
+      q   <- arbitrary
+      o   <- arbitrary
+      -- See "Note: Convert to Source type and back in generator" above
+      Just t <- (valTypeOfType . typeOfValType) <$> arbitrary
+      return $ QueryWithFeature q (Just now) o t nm tm
+
+
 
 qwfPretty :: QueryWithFeature -> String
 qwfPretty qwf
@@ -276,3 +210,186 @@ qwfDesugar qwf
 qwfConvertToCore qwf qt
  = freshtest "core" $ convertQueryTop (qwfFeatureMap qwf)
   (runIdentity $ freshtest "reify" $ reifyPossibilityQT qt)
+
+
+
+-- Generate well-formed
+type Var = CB.Name T.Variable
+
+data TypedGenInfo
+ = TypedGenInfo
+ { tgiVars :: Map.Map Var TestTemporality
+ , tgiTemp :: TestTemporality }
+
+data TestTemporality
+ = TTPure | TTElt | TTAgg
+ deriving Eq
+
+instance Arbitrary TestTemporality where
+ arbitrary = elements [ TTPure, TTElt, TTAgg ]
+
+availableTT :: TypedGenInfo -> TestTemporality -> Bool
+availableTT tgi tt
+ =  tt == TTPure
+ || tt == tgiTemp tgi
+
+genVar :: TypedGenInfo -> Gen Var
+genVar tgi
+ = if   Map.null found
+   then arbitrary
+   else oneof_vals $ Map.keys found
+ where
+  found
+   = Map.filter (availableTT tgi)
+   $ tgiVars tgi
+
+genExp :: TypedGenInfo -> Gen (Exp () T.Variable)
+genExp tgi
+ = oneof_sized
+    [ Var    () <$> genVar tgi
+    , Prim   () <$> arbitrary ]
+    [ (simplifyNestedX . Nested ()) <$> genQuery tgi
+    , App    () <$> genExp tgi <*> genExp tgi
+    , Case   () <$> genExp tgi
+                <*> listOf1 (genCase tgi)
+    , unop
+    , binop
+    ]
+
+  where
+   unop
+    = App ()
+    <$> (Prim () <$> operator_unary)
+    <*> genExp tgi
+
+   binop
+    = App ()
+    <$> (App ()
+            <$> (Prim () <$> operator_bin)
+            <*> genExp tgi)
+    <*> genExp tgi
+
+   operator_bin
+    = oneof_vals
+        [ Op    (ArithDouble Div)
+        , Op    (ArithBinary Mul)
+        , Op    (ArithBinary Add)
+        , Op    (ArithBinary Sub)
+        , Op    (ArithBinary Pow)
+        , Op    (Relation Gt)
+        , Op    (Relation Eq)
+        , Op    (LogicalBinary And)
+        , Op    (LogicalBinary Or)
+        , Op    (TimeBinary DaysBefore)
+        , Op    (TimeBinary DaysAfter)
+        , Op    (TimeBinary MonthsBefore)
+        , Op    (TimeBinary MonthsAfter)
+        , Fun   (BuiltinTime DaysBetween)
+        , Fun   (BuiltinTime DaysEpoch)
+        , Fun   (BuiltinData Seq) ]
+
+   operator_unary
+    = oneof_vals
+        [ Op (ArithUnary Negate)
+        , Op (LogicalUnary Not)
+        , Fun (BuiltinMap MapKeys)
+        , Fun (BuiltinMap MapValues) ]
+
+
+genCase :: TypedGenInfo -> Gen (Pattern T.Variable, Exp () T.Variable)
+genCase tgi
+ = do (pat,bs) <- genPat
+      let tgi' = tgi { tgiVars = tgiVars tgi `Map.union` bs }
+      x <- genExp tgi'
+      return (pat,x)
+ where
+  genPat
+   = oneof_sized [ return (PatDefault, Map.empty), genPatVar ]
+                 [ genPatCon ]
+
+  genPatVar
+   = do a <- arbitrary
+        return (PatVariable a, Map.singleton a (tgiTemp tgi))
+
+  genPatCon
+   = do con <- arbitrary
+        args <- vectorOf (arityOfConstructor con) genPat
+        let (args',bss) = List.unzip args
+        return (PatCon con args', Map.unions bss)
+
+
+genQuery :: TypedGenInfo -> Gen (Query () T.Variable)
+genQuery toptgi
+ = do -- Generate a very small number of contexts.
+      -- It doesn't matter that the numbers are small, because they can be nested
+      -- and any nesting will be simplified away
+      num <- oneof_sized [return 0] [return 1]
+      (ctxs,tgi') <- genCtxs num toptgi []
+      x <- genExp tgi'
+      return (Query (reverse ctxs) x)
+
+ where
+  genCtxs :: Int -> TypedGenInfo -> [Context () T.Variable] -> Gen ([Context () T.Variable], TypedGenInfo)
+  genCtxs 0 tgi ss = return (ss, tgi)
+  genCtxs n tgi ss
+   = do (ctx',tgi') <- genCtx tgi
+        genCtxs (n-1) tgi' (ctx' : ss)
+
+  genCtx :: TypedGenInfo -> Gen (Context () T.Variable, TypedGenInfo)
+  genCtx tgi
+   | TTAgg <- tgiTemp tgi
+   = genCtxAgg tgi
+   | otherwise
+   = genLet tgi
+
+  genCtxAgg :: TypedGenInfo -> Gen (Context () T.Variable, TypedGenInfo)
+  genCtxAgg tgi
+   = oneof_sized
+      [ genWindow tgi
+      , nobinds tgi . Latest   () . abs <$> arbitrary ]
+      [ nobinds tgi . GroupBy  () <$> genExp tgi
+      , nobinds tgi . Distinct () <$> genExp tgi
+      , nobinds tgi . Filter   () <$> genExp tgi
+      , genFold tgi
+      , genLet tgi
+      , genGroupFold tgi
+      ]
+
+  genWindow tgi
+   = do pre <- arbitrary
+        post <- arbitrary
+        return (Windowed () pre post, tgi)
+
+  genFold tgi
+   = do v <- arbitrary :: Gen Var
+        f <- Fold v <$> genExp tgi { tgiTemp = TTPure } <*> genExp tgi { tgiTemp = TTElt, tgiVars = Map.insert v TTElt $ tgiVars tgi } <*> arbitrary
+        let tgi' = tgi { tgiVars = Map.insert v TTAgg $ tgiVars tgi }
+        return (LetFold () f, tgi')
+  genLet tgi
+   = do v <- arbitrary :: Gen Var
+        tt' <- genTemporality tgi
+        l <- Let () v <$> genExp tgi { tgiTemp = tt' }
+        let tgi' = tgi { tgiVars = Map.insert v tt' $ tgiVars tgi }
+        return (l,tgi')
+  genGroupFold tgi
+   = do k <- arbitrary :: Gen Var
+        v <- arbitrary :: Gen Var
+        let tgi' = tgi { tgiVars = Map.insert k TTElt $ Map.insert v TTElt $ tgiVars tgi }
+
+        (Query ctxs x) <- genQuery tgi
+        grpky <- genExp tgi
+        let grp' = Query (GroupBy () grpky : ctxs) x
+
+        let g = GroupFold () k v (Nested () grp')
+        return (g, tgi')
+
+  nobinds tgi x
+   = (x, tgi)
+
+genTemporality :: TypedGenInfo -> Gen TestTemporality
+genTemporality tgi
+ = case tgiTemp tgi of
+    TTPure -> return TTPure
+    TTElt  -> elements [TTPure, TTElt]
+    TTAgg  -> elements [TTPure, TTElt, TTAgg]
+
