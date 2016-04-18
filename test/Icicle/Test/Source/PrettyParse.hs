c06ddfef1a85b655733e61615125eb7802940450
Amos Robinson <amos.robinson@gmail.com>
2016-04-18 15:06:13 +1000

    Test.Source.PrettyParse: only test on well-formed programs with fully applied operators

--- test/Icicle/Test/Source/PrettyParse.hs
+++ test/Icicle/Test/Source/PrettyParse.hs
@@ -21,15 +21,19 @@ import           Icicle.Source.Query
 
 import           Icicle.Source.PrettyAnnot
 
-prop_parse_pretty_same :: QueryWithFeature -> Property
-prop_parse_pretty_same qwf
+prop_parse_pretty_same :: Property
+prop_parse_pretty_same
+ -- Ill-typed programs will be printed/parsed incorrectly if they have operators
+ -- with the wrong number of arguments - for example (!) applied to no arguments.
+ -- However, by setting the tableflip ratio to 0,
+ -- it will generate only the correct number of arguments
+ = forAll (genQueryWithFeatureTypedGen 0) check_pretty
+
+check_pretty :: QueryWithFeature -> Property
+check_pretty qwf
  = counterexample pp
  $ counterexample pp'
- -- Ill-typed programs will be printed/parsed incorrectly if they have operators
- -- with the wrong number of arguments - for example (!) applied to no arguments
- $ case qwfCheck qwf of
-    Left  _ -> property Discard
-    Right _ -> parsed' === Right q
+ $ parsed' === Right q
  where
   q  = qwfQueryTop qwf
   pp = show $ pretty q
