6a2e73cb75e1886da6f56d7372748840c92f3a70
Tran Ma <tran@defma.in>
2017-02-27 15:45:04 +1100

    Sea: make array_t when buf_t is used

--- data/sea/51-piano.h
+++ data/sea/51-piano.h
@@ -2,15 +2,14 @@
 
 typedef struct piano piano_t;
 
-static int64_t piano_max_count (
+int64_t piano_max_count (
     piano_t *piano
   );
 
-static error_t piano_lookup (
+error_t piano_lookup (
     piano_t *piano
   , const uint8_t *needle_id
   , size_t needle_id_size
   , int64_t *out_count
   , const int64_t **out_times
   );
-
